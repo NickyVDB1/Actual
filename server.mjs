3b41872a3fed9ffc8ef971b0da2f57492a53fc23
Vanden2 <nicky.vandenbussche@numble.be>
2024-07-30 00:07:02 +0200

    ergt

--- server.mjs
+++ server.mjs
@@ -37,5 +37,5 @@ app.get('/RunBanksync', async (req, res) => {
   }
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Server running on port ${port}`));
